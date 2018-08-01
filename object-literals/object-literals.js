abbc7d5a017b2707b032099c894c79270897cafa
aboud20-meet <aboud20@meet.mit.edu>
2018-08-01 14:11:50 +0300

    shshshs

--- object-literals/object-literals.js
+++ object-literals/object-literals.js
@@ -1,1 +1,10 @@
 // Create your 'me' object literal here!
+var me= {
+ first name:"rawand",
+ last name:"Mukarker",
+ favorite food:["Malfuf","shawerma","fries"],
+age: 16,
+pic:"https://www.google.com/search?q=oh+wonder&client=ubuntu&hs=aOr&channel=fs&source=lnms&tbm=isch&sa=X&ved=0ahUKEwj87ejG1MvcAhVG66QKHST8BPAQ_AUICigB&biw=1299&bih=639#imgrc=IA5cRBRXqB_BAM:",
+
+}
+document.write (JSON.stringify(me))
